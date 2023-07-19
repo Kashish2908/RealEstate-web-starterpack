56182e22cdfc07469f4003b5513dda6d5c04343b
Ankit-mandli <110595460+Ankit-mandli@users.noreply.github.com>
2023-07-19 21:18:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import Header from "./components/header";
-import './components/header.css'
+import Header from "./components/Header/header";
+import Hero from "./components/Hero/Hero"
+// import './components/header.css'
+
+import Back_destination from './components/Back_destination/Back_destination';
 
 function App() {
   return (
+    <>
    <Header/>
+   <Hero/>
+   <Back_destination/>
+   
+    </>
   );
 }
 
