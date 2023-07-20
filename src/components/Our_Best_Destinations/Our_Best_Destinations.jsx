6b63bcd3964cd50026973a0428cbe8f6d716417a
Ankit <ankitmandli2001@gmail.com>
2023-07-20 15:42:11 +0530

    Our reviews

--- src/components/Our_Best_Destinations/Our_Best_Destinations.jsx
+++ src/components/Our_Best_Destinations/Our_Best_Destinations.jsx
@@ -7,50 +7,116 @@ import img4 from "../../Assets/Back_destinations_images/slide_image_4.jpg";
 import img5 from "../../Assets/Back_destinations_images/slide_image_5.jpg";
 import img6 from "../../Assets/Back_destinations_images/slide_image_6.jpg";
 
+import profile1 from '../../Assets/Our_best_reviews_images/sad.png';
+
 const Our_Best_Destinations = () => {
   return (
-    <div className="container mb-5">
-        <h1 className="font-coffee text-center mb-5">Our Reviews</h1>
+    <div className="container review-main">
+      <h1 className="font-coffee text-center mb-5">Our Reviews</h1>
       <div className="review-wrapper">
         <div class="book">
-        <div class="card1">
-      <div class="img-bx">
-        <img src="https://i.postimg.cc/dQ7zWbS5/img-4.jpg" alt="img" />
-      </div>
-      <div class="content">
-        <div class="detail">
-          <h2>Emilia Roy<br /><span>Senior Designer</span></h2>
-          <ul class="sci">
-            <li>
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-            </li>
-            <li>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-            </li>
-            <li>
-              <a href="#"><i class="fab fa-linkedin-in"></i></a>
-            </li>
-            <li>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+          <div class="team-boxed">
+            <div class="container">
+              <div class="row people">
+                <div class="col-md-12 col-lg-12 item">
+                  <div class="box">
+                    <img class="rounded-circle" src={profile1} />
+                    <h3 class="name">Ben Johnson</h3>
+                    <p class="title">Musician</p>
+                    <p class="description">
+                      Aenean tortor est, vulputate quis leo in, vehicula rhoncus
+                      lacus. Praesent aliquam in tellus eu gravida. Aliquam
+                      varius finibus est, et interdum justo suscipit id. Etiam
+                      dictum feugiat tellus, a semper massa.{" "}
+                    </p>
+                    <div class="social">
+                      <a href="#">
+                        <i class="fa fa-facebook-official"></i>
+                      </a>
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="cover">
             <img src={img1} alt="" />
           </div>
         </div>
         <div class="book">
-          <p>Hello</p>
+          <div class="team-boxed">
+            <div class="container">
+              <div class="row people">
+                <div class="col-md-12 col-lg-12 item">
+                  <div class="box">
+                    <img class="rounded-circle" src={profile1} />
+                    <h3 class="name">Ben Johnson</h3>
+                    <p class="title">Musician</p>
+                    <p class="description">
+                      Aenean tortor est, vulputate quis leo in, vehicula rhoncus
+                      lacus. Praesent aliquam in tellus eu gravida. Aliquam
+                      varius finibus est, et interdum justo suscipit id. Etiam
+                      dictum feugiat tellus, a semper massa.{" "}
+                    </p>
+                    <div class="social">
+                      <a href="#">
+                        <i class="fa fa-facebook-official"></i>
+                      </a>
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="cover">
-            <p>Hover Me</p>
+            <img src={img4} alt="" />
           </div>
         </div>
         <div class="book">
-          <p>Hello</p>
+          <div class="team-boxed">
+            <div class="container">
+              <div class="row people">
+                <div class="col-md-12 col-lg-12 item">
+                  <div class="box">
+                    <img class="rounded-circle" src={profile1} />
+                    <h3 class="name">Ben Johnson</h3>
+                    <p class="title">Musician</p>
+                    <p class="description">
+                      Aenean tortor est, vulputate quis leo in, vehicula rhoncus
+                      lacus. Praesent aliquam in tellus eu gravida. Aliquam
+                      varius finibus est, et interdum justo suscipit id. Etiam
+                      dictum feugiat tellus, a semper massa.{" "}
+                    </p>
+                    <div class="social">
+                      <a href="#">
+                        <i class="fa fa-facebook-official"></i>
+                      </a>
+                      <a href="#">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="cover">
-            <p>Hover Me</p>
+            <img src={img6} alt="" />
           </div>
         </div>
       </div>
