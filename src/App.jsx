915b49c6ec0843d5f2888de4079be85c101c3521
Kashish2908 <kneekhra29@gmail.com>
2023-07-20 12:18:16 +0530

    services added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import 'remixicon/fonts/remixicon.css'
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services"
 import Footer from "./components/footer/footer"
 
 import Back_destination from './components/Back_destination/Back_destination';
@@ -12,6 +13,7 @@ function App() {
    <Header/>
    <Hero/>
    <Back_destination/>
+   <Services/>
    <Footer />
    
     </>
