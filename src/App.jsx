3bd2326d109c02ce2748711c72d97a5e69d09f1b
Kashish2908 <kneekhra29@gmail.com>
2023-07-20 15:05:13 +0530

    created our package folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import 'remixicon/fonts/remixicon.css'
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/Hero";
-import Services from "./components/Services/Services"
-import Footer from "./components/footer/footer"
+import Services from "./components/Services/Services";
+import Footer from "./components/footer/footer";
+import ourPackages from './components/our-packages/our-packages';
 
 import Back_destination from './components/Back_destination/Back_destination';
 import Our_Best_Destinations from './components/Our_Best_Destinations/Our_Best_Destinations';
@@ -14,6 +15,7 @@ function App() {
    <Header/>
    <Hero/>
    <Back_destination/>
+   <ourPackages/>
    <Services/>
    <Our_Best_Destinations/>
    <Footer />
