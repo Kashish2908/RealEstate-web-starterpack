fdb3305dbd7b3494e311ea84d87f2db806ff0de6
Kashish2908 <kneekhra29@gmail.com>
2023-07-24 11:53:51 +0530

    animation removed

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./header.css";
+// import "../../../index"
 
 const Header = () => {
   const menuData = [
